1bbeeb099935a49cb06070c4eca038839d37a497
StarWalkin <sjtu.cs.gy@gmail.com>
2023-10-13 13:07:31 +0800

    Update gigignore

--- build1/CMakeFiles/LSTM_inf_in_C.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for LSTM_inf_in_C.
-# This may be replaced when dependencies are built.
--- build1/CMakeFiles/LSTM_inf_in_C.dir/initialization.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/LSTM_inf_in_C.dir/initialization.c.o: \
-  /Users/yuan/Desktop/科研竞赛/ANL-瑞萨项目/LSTM_inf_in_C/initialization.c \
-  /Users/yuan/Desktop/科研竞赛/ANL-瑞萨项目/LSTM_inf_in_C/initialization.h
